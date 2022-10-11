4bb7b23060abb4e0bafb0f7005a0c23bc9df60ff
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-12 06:57:59 +0900

    20221012

--- Boda/html/assets/js/headermenu.js
+++ Boda/html/assets/js/headermenu.js
@@ -1,40 +1,21 @@
-// header hover 
-const sub = document.querySelectorAll(".header__center .menu li .sub");
-const menu = document.querySelectorAll(".header__center .menu li a");
-const header = document.querySelector(".header__center");
+// header hover
+const header = document.querySelector(".header__menu");
+const submenu = document.querySelectorAll(".header__menu ul li .sub");
 
 header.addEventListener("mouseover", () => {
-    header.classList.remove("height");
-    sub.forEach((e) => {
-        e.style.display = "block";
-    })
-})
+    submenu.forEach((el) => {
+        el.classList.remove("blind");
+    });
+});
 header.addEventListener("mouseout", () => {
-    header.classList.add("height");
-    sub.forEach((e) => {
-        e.style.display = "none";
-    })
-})
-
-
-// menu.forEach((e) => {
-//     e.addEventListener("mouseover", () => {
-//         header.classList.remove("height");
-//         sub.forEach((e) => {
-//             e.style.display = "block";
-//         })
-//     })
-//     e.addEventListener("mouseout", () => {
-//         header.classList.add("height");
-//         sub.forEach((e) => {
-//             e.style.display = "none";
-//         })
-//     })
-// })
+    submenu.forEach((el) => {
+        el.classList.add("blind");
+    });
+});
 
 // smallmenu
-const menuBtn = document.querySelector(".header__right > svg");
-const menuBtnClose = document.querySelector(".header__right > svg:nth-child(3)");
+const menuBtn = document.querySelector(".header__right .ham");
+const menuBtnClose = document.querySelector(".header__right .ham__close");
 const smallmenu = document.querySelector(".smallmenu");
 
 menuBtn.addEventListener("click", () => {
@@ -49,3 +30,17 @@ menuBtnClose.addEventListener("click", () => {
     menuBtn.classList.remove("blind");
 });
 
+// media ham
+const MBHam = document.querySelector(".ham__mobile");
+const MBHamClose = document.querySelector(".ham__mobile__close");
+const MBmenu = document.querySelector(".mobilemenu");
+MBHam.addEventListener("click", () => {
+    MBHam.classList.add("blind");
+    MBHamClose.classList.remove("blind");
+    MBmenu.classList.add("overlay");
+});
+MBHamClose.addEventListener("click", () => {
+    MBHam.classList.remove("blind");
+    MBHamClose.classList.add("blind");
+    MBmenu.classList.remove("overlay");
+});
